5f9098972d819ec7aaf1b2e3ff8fd713a75a4067
Aditya <adityakurhade1000@gmail.com>
2025-06-28 12:36:07 +0530

    frontend code successfully done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,43 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import "prismjs/themes/prism-tomorrow.css"
+import prismjs from 'prismjs'
+import Editor from "react-simple-code-editor"
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [code, setCode] = useState(`function sum(){
+  return 1 + 2;
+}`)
+
+  useEffect(() => {
+    // Highlight code blocks
+    prismjs.highlightAll()
+  })
 
   return (
     <>
     <main>
       <div className="left">
-        <div className="code"></div>
+        <div className="code">
+          <Editor
+            value={code}
+            onValueChange={setCode}
+            highlight={code => prismjs.highlight(code, prismjs.languages.js)}
+            padding={10}
+            className="editor"
+
+            style={{
+              fontFamily: '"Fira code", "Fira Mono", monospace', 
+              fontSize: 20,
+              color: '#f8f8f2',
+              height: '100%',
+              width: '100%',
+              border: '1px solid #44475a',
+              borderRadius: '5px',
+            }}
+          />
+        </div>
         <div className="review">Review</div>
       </div>
       <div className="right"></div>
