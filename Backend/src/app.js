ca25bea85b2ee5759bf50ac8484ef911c8313225
Aditya <adityakurhade1000@gmail.com>
2025-06-28 09:54:08 +0530

    code updated

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const aiRoute = require('./routes/ai.route');
 
 const app = express();
 
 app.get("/" , (req, res) => {
     res.send("Welcome to the AI-Powered Code Reviewer!");
 })
+
+app.use("/ai", aiRoute);
+app.use(express.json()); // Middleware to parse JSON bodies
 
 module.exports = app;
