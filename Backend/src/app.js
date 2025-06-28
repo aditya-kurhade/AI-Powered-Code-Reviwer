382f43a3b0327f9a1819f60389ea7b18554439aa
Aditya <adityakurhade1000@gmail.com>
2025-06-28 17:16:39 +0530

    cors is imported

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const aiRoute = require('./routes/ai.route');
+const cors = require('cors');
+
 
 const app = express();
+
+app.use(cors()); // Enable CORS for all routes
+app.use(express.json()); // Middleware to parse JSON bodies
 
 app.get("/" , (req, res) => {
     res.send("Welcome to the AI-Powered Code Reviewer!");
 })
 
 app.use("/ai", aiRoute);
-app.use(express.json()); // Middleware to parse JSON bodies
+
 
 module.exports = app;
